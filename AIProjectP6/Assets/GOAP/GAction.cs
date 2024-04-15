9bc513b72ef581f785ef093bb3a9188c5ec6e639
SamuelM24 <beniciomedina11@gmail.com>
2024-04-14 23:32:47 -0700

    ww

--- AIProjectP6/Assets/GOAP/GAction.cs
+++ AIProjectP6/Assets/GOAP/GAction.cs
@@ -2,88 +2,71 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public abstract class GAction : MonoBehaviour {
+public abstract class GAction : MonoBehaviour 
+{
 
-    // Name of the action
+
     public string actionName = "Action";
-    // Cost of the action
     public float cost = 1.0f;
-    // Target where the action is going to take place
     public GameObject target;
-    // Store the tag
     public string targetTag;
-    // Duration the action should take
     public float duration = 0.0f;
-    // An array of WorldStates of preconditions
     public WorldState[] preConditions;
-    // An array of WorldStates of afterEffects
     public WorldState[] afterEffects;
-    // The NavMEshAgent attached to the agent
     public NavMeshAgent agent;
-    // Dictionary of preconditions
     public Dictionary<string, int> preconditions;
-    // Dictionary of effects
     public Dictionary<string, int> effects;
-    // State of the agent
     public WorldStates agentBeliefs;
-    // Access our inventory
     public GInventory inventory;
     public WorldStates beliefs;
-    // Are we currently performing an action?
     public bool running = false;
 
-    // Constructor
-    public GAction() {
+    public GAction() 
+    {
 
-        // Set up the preconditions and effects
         preconditions = new Dictionary<string, int>();
         effects = new Dictionary<string, int>();
     }
 
-    private void Awake() {
+    private void Awake() 
+    {
 
-        // Get hold of the agents NavMeshAgent
         agent = this.gameObject.GetComponent<NavMeshAgent>();
 
-        // Check if there are any preConditions in the Inspector
-        // and add to the dictionary
-        if (preConditions != null) {
-
-            foreach (WorldState w in preConditions) {
-
-                // Add each item to our Dictionary
+        if (preConditions != null) 
+        {
+            foreach (WorldState w in preConditions) 
+            {
                 preconditions.Add(w.key, w.value);
             }
         }
 
-        // Check if there are any afterEffects in the Inspector
-        // and add to the dictionary
-        if (afterEffects != null) {
+        if (afterEffects != null) 
+        {
 
-            foreach (WorldState w in afterEffects) {
-
-                // Add each item to our Dictionary
+            foreach (WorldState w in afterEffects) 
+            {
                 effects.Add(w.key, w.value);
             }
         }
-        // Populate our inventory
         inventory = this.GetComponent<GAgent>().inventory;
-        // Get our agents beliefs
         beliefs = this.GetComponent<GAgent>().beliefs;
     }
 
-    public bool IsAchievable() {
+    public bool IsAchievable() 
+    {
 
         return true;
     }
 
-    //check if the action is achievable given the condition of the
-    //world and trying to match with the actions preconditions
-    public bool IsAhievableGiven(Dictionary<string, int> conditions) {
+    public bool IsAhievableGiven(Dictionary<string, int> conditions) 
+    {
 
-        foreach (KeyValuePair<string, int> p in preconditions) {
+        foreach (KeyValuePair<string, int> p in preconditions) 
+        {
 
-            if (!conditions.ContainsKey(p.Key)) {
+            if (!conditions.ContainsKey(p.Key)) 
+            {
 
                 return false;
             }
--- AIProjectP6/Assets/GOAP/GWorld.cs
+++ AIProjectP6/Assets/GOAP/GWorld.cs
@@ -1,82 +1,67 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public sealed class GWorld {
+public sealed class GWorld
+{
 
-    // Our GWorld instance
     private static readonly GWorld instance = new GWorld();
-    // Our world states
     private static WorldStates world;
-    // Queue of patients
     private static Queue<GameObject> patients;
-    // Queue of cubicles
     private static Queue<GameObject> cubicles;
 
-    static GWorld() {
-
-        // Create our world
+    static GWorld()
+    {
         world = new WorldStates();
-        // Create patients array
         patients = new Queue<GameObject>();
-        // Create cubicles array
         cubicles = new Queue<GameObject>();
-        // Find all GameObjects that are tagged "Cubicle"
         GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cubicle");
-        // Then add them to the cubicles Queue
-        foreach (GameObject c in cubes) {
-
+        foreach (GameObject c in cubes)
+        {
             cubicles.Enqueue(c);
         }
 
-        // Inform the state
-        if (cubes.Length > 0) {
+        if (cubes.Length > 0)
+        {
             world.ModifyState("FreeCubicle", cubes.Length);
         }
 
-        // Set the time scale in Unity
         Time.timeScale = 5.0f;
     }
 
-    private GWorld() {
+    private GWorld()
+    {
 
     }
 
-    // Add patient
-    public void AddPatient(GameObject p) {
-
-        // Add the patient to the patients Queue
+    public void AddPatient(GameObject p)
+    {
         patients.Enqueue(p);
     }
 
-    // Remove patient
-    public GameObject RemovePatient() {
-
+    public GameObject RemovePatient()
+    {
         if (patients.Count == 0) return null;
         return patients.Dequeue();
     }
 
-    // Add cubicle
-    public void AddCubicle(GameObject p) {
-
-        // Add the patient to the patients Queue
+    public void AddCubicle(GameObject p)
+    {
         cubicles.Enqueue(p);
     }
 
-    // Remove cubicle
-    public GameObject RemoveCubicle() {
-
-        // Check we have something to remove
+    public GameObject RemoveCubicle()
+    {
         if (cubicles.Count == 0) return null;
         return cubicles.Dequeue();
     }
 
-    public static GWorld Instance {
-
+    public static GWorld Instance
+    {
         get { return instance; }
     }
 
-    public WorldStates GetWorld() {
-
+    public WorldStates GetWorld()
+    {
         return world;
     }
 }
